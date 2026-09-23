916ddc9f366a06bd75a66f4afefe51412fca7186
samvelgh <samvel_ghazaryan2@edu.aua.am>
2026-09-23 17:48:51 +0000

    HW4

---
+++ HW4/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "math_utils.h"
+
+int main()
+{
+    int number = 5;
+
+    int result = square(number);
+
+    printf("Square of %d is %d\n", number, result);
+
+    return 0;
+}
---
+++ HW4/math_utils.c
@@ -0,0 +1,6 @@
+#include "math_utils.h"
+
+int square(int number)
+{
+    return number * number;
+}
